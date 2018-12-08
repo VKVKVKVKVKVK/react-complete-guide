6d5ea878b215aec1ecd9568c7ab107791944361e
Vladimir.Kopecky <vladimir.kopecky@epita.fr>
2018-12-08 21:38:51 +0100

    Added props to class Person

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'John', age: 27 },
+      { name: 'Clara', age: 24 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working !</p>
-        <Person />
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >I like video-games</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >I like painting</Person>
       </div>
     );
   }
