316bff18ac54ed2b8b5200081a7dbac2d66f457b
Vladimir.Kopecky <vladimir.kopecky@epita.fr>
2019-02-13 11:08:45 +0100

    Added style and name handlers functions

--- src/App.js
+++ src/App.js
@@ -10,15 +10,44 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 32 },
+        { name: 'Jenny', age: 25 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 70 },
+        { name: 'Jenny', age: 25 }
+      ]
+    })
+  }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working !</p>
-        <button>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Johnny')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Vlado')}
+          changed={this.nameChangedHandler}
         >I like video-games</Person>
         <Person
           name={this.state.persons[1].name}
